48524ac453adc9031c92b62edd1a1bf481aa4683
Max McCain <itsmaximuk@gmail.com>
2024-10-10 20:00:58 +0900

    updated game loop

--- src/name/main/GamePanel.java
+++ src/name/main/GamePanel.java
@@ -46,35 +46,57 @@ public class GamePanel extends JPanel implements Runnable{
   }
 
   @Override
+  // public void run() {
+
+  //   double drawInterval = 1000000000/FPS;
+  //   double nextDrawTime = System.nanoTime() + drawInterval;
+
+  //   while(gameThread != null) {
+
+  //     // Update player position
+  //     update();
+  //     // Draw the screen with the updated information
+  //     repaint();
+
+
+  //     try {
+  //       double remainingTime = nextDrawTime - System.nanoTime();
+  //       remainingTime = remainingTime/1000000;
+
+  //       if(remainingTime < 0) {
+  //         remainingTime = 0;
+  //       }
+
+  //       Thread.sleep((long)remainingTime);
+
+  //       nextDrawTime += drawInterval;
+
+  //     } catch (InterruptedException e) {
+  //       e.printStackTrace();
+  //     }
+
+  //   }
+  // }
+
+  // refactored game loop
   public void run() {
 
     double drawInterval = 1000000000/FPS;
-    double nextDrawTime = System.nanoTime() + drawInterval;
+    double delta = 0;
+    long lastTime = System.nanoTime();
+    long currentTime;
 
     while(gameThread != null) {
 
-      // Update player position
-      update();
-      // Draw the screen with the updated information
-      repaint();
+      currentTime = System.nanoTime();
+      delta += (currentTime - lastTime) / drawInterval;
+      lastTime = currentTime;
 
-
-      try {
-        double remainingTime = nextDrawTime - System.nanoTime();
-        remainingTime = remainingTime/1000000;
-
-        if(remainingTime < 0) {
-          remainingTime = 0;
-        }
-
-        Thread.sleep((long)remainingTime);
-
-        nextDrawTime += drawInterval;
-
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+      if(delta >= 1) {
+        update();
+        repaint();
+        delta--;
       }
-
     }
   }
 
