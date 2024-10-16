663234c11db6451e202de44546130a307746331d
Max McCain <itsmaximuk@gmail.com>
2024-10-16 19:31:41 +0900

    added objects class

--- src/name/main/GamePanel.java
+++ src/name/main/GamePanel.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import java.awt.Dimension;
@@ -36,7 +37,9 @@ public class GamePanel extends JPanel implements Runnable{
   KeyHandler keyH = new KeyHandler();
   Thread gameThread;
   public CollisionChecker cChecker = new CollisionChecker(this);
+  public AssetSetter aSetter = new AssetSetter(this);
   public Player player = new Player(this,keyH);
+  public SuperObject obj[] = new SuperObject[10];
 
   public GamePanel() {
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -44,6 +47,10 @@ public class GamePanel extends JPanel implements Runnable{
     this.setDoubleBuffered(true);
     this.addKeyListener(keyH);
     this.setFocusable(true);
+  }
+
+  public void setupGame() {
+    aSetter.setObject();
   }
 
   public void startGameThread() {
@@ -100,8 +107,16 @@ public class GamePanel extends JPanel implements Runnable{
     super.paintComponent(g);
 
     Graphics2D g2 = (Graphics2D)g;
-
+    // Tile
     tileM.draw(g2);
+
+    // Object
+    for(int i = 0; i < obj.length; i++) {
+      if(obj[i] != null) {
+        obj[i].draw(g2, this);
+      }
+    }
+    // Player
     player.draw(g2);
 
     g2.dispose();
